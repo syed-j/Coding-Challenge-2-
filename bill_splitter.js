65c950edf31c20bda8d8ed94b75e816f86eb6e74
Syed Jafri <ijtabajafripp@gmail.com>
2024-09-08 14:05:01 -0400

    Implement and populate arrays for bills, tips, and totals

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,17 +1,30 @@
 // Coding Challenge 2
 
+// Step 1 
 const bill = 300;
 const tip = bill >= 100 ? 50 : 20;
 const total = bill + tip;
 
+// Step 2
 console.log(`The tip is $${tip}`);
 console.log(`The total value of the bill including the tip is $${total}`);
 
+//Step 3
 function calculateTip(bills) {
     return bills >= 100 ? 50 : 20;
 }
 
 const bills = 100;
-const tips = calculateTip(bills);
+const Tips = calculateTip(bills);
 
-console.log(`The tip for a bill of $${bills} is $${tips}`);
+console.log(`The tip for a bill of $${bills} is $${Tips}`);
+
+// Step 4
+const Bill = [275, 40, 430, 125, 555, 44];
+const tips = Bill.map(calculateTip);
+
+const totals = Bill.map((Bill, index) => Bill + tips[index]);
+
+console.log('Bill:', Bill);
+console.log('tips:', tips);
+console.log('totals:', totals);
